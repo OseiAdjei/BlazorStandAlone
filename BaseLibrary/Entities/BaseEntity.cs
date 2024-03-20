b42ed1a054a50ddad4118b3f3eee56b77c92464a
Edward Adjei <wikieddi@icloud.com>
2024-03-20 15:34:30 +0000

    moved relationship to BaseEntity and deleted Relationship

--- BaseLibrary/Entities/BaseEntity.cs
+++ BaseLibrary/Entities/BaseEntity.cs
@@ -8,9 +8,10 @@ namespace BaseLibrary.Entities
     {
         public int Id { get; set; }
         public string? Name { get; set; }
+
         //One to Many Relationship
         [JsonIgnore]
-        public List<Employee> Employees { get; set; }
+        public List<Employee>? Employees { get; set; }
 
 
     }
