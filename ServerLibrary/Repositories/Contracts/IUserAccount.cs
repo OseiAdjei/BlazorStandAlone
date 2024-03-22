c0597e5b482eb38b59ba6a0487417f1a5b280db4
Edward Adjei <wikieddi@icloud.com>
2024-03-22 10:54:11 +0000

    responses for General and login and interfaces for these as well

--- ServerLibrary/Repositories/Contracts/IUserAccount.cs
+++ ServerLibrary/Repositories/Contracts/IUserAccount.cs
@@ -1,4 +1,5 @@
 ﻿using BaseLibrary.DTOs;
+using BaseLibrary.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace ServerLibrary.Repositories.Contracts
 {
-    public class IUserAccount
+    public interface IUserAccount
     {
         Task<GeneralResponse> CreateAsync(Register user);
 
