af983316d821ace94bfe4cc4d689b1c69d904898
Edward Adjei <wikieddi@icloud.com>
2024-03-25 18:18:34 +0000

    check well

---
+++ ClientLibrary/Helpers/Serializations.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ClientLibrary.Helpers
+{
+    internal class Serializations
+    {
+        public static string SerializeObj<T>(T modelObject) => JsonSerializer.Serialize(modelObject);
+        public static T DeserializeJsonString<T>(string jsonString) => JsonSerializer.Deserialize<T>(jsonString());
+        public static IList<T> DeserializeJsonString<T>(string jsonString) => JsonSerializer.Deserialize<IList>(jsonString);
+    }
+}
+    
