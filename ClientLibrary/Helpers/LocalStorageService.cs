7404544b2a439e8830055f75cec32b93d9f638fa
Edward Adjei <wikieddi@icloud.com>
2024-03-27 14:25:22 +0000

    storage service logic for Get,Set and Remove Token

--- ClientLibrary/Helpers/LocalStorageService.cs
+++ ClientLibrary/Helpers/LocalStorageService.cs
@@ -11,6 +11,8 @@ namespace ClientLibrary.Helpers
     {
         private const string StorageKey = "authentication-token";
         public async Task<string> GetToken() => await localStorageService.GetItemAsStringAsync(StorageKey);
+        public async Task SetToken (string token) => await localStorageService.SetItemAsStringAsync(StorageKey, token);
+        public async Task RemoveToken() => await localStorageService.RemoveItemAsync(StorageKey);
 
     }
 }
