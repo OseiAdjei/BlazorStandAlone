5d487d394d5c7ceba720e2e903497c8f7eb11290
kwabs digital <kwabs digital@DESKTOP-F8MDC1K>
2024-04-01 07:01:02 +0000

    implementing SignInAsync

--- ClientLibrary/Services/Implementations/UserAccountService.cs
+++ ClientLibrary/Services/Implementations/UserAccountService.cs
@@ -23,19 +23,25 @@ namespace ClientLibrary.Services.Implementations
 
             return await result.Content.ReadFromJsonAsync<GeneralResponse>()!;
         }
+        public async Task<LoginResponse> SignInAsync(Login user)
+        {
+            var httpClient = getHttpClient.GetPublicHttpClient();
+            var result = await httpClient.PostAsJsonAsync($"{AuthUrl}/login", user);
+            if (!result.IsSuccessStatusCode) return new LoginResponse(false, "Error occurred");
 
+            return await result.Content.ReadFromJsonAsync<LoginResponse>()!;
+
+        }
         public Task<LoginResponse> RefreshTokenAsync(RefreshToken token)
         {
             throw new NotImplementedException();
         }
 
-        public Task<LoginResponse> SignInAsync(Login user)
+        public async Task<WeatherForecast[]> GetWeatherForecast()
         {
-            throw new NotImplementedException();
-        }
-        public Task<WeatherForecast[]> GetWeatherForecast()
-        {
-            throw new NotImplementedException();
+            var httpClient = getHttpClient.GetPrivateHttpClient();
+            var result = await httpClient.GetFromJsonAsync<WeatherForecast[]>("api/weatherforecast");
+            return result;
         }
     }
 }
